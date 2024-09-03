511fa8e02e7e749b114172c9de0b25282a1236e1
jquie <jacquelynphan03@gmail.com>
2024-09-03 10:05:48 -0700

    class instructions

---
+++ Printing.java
@@ -0,0 +1,15 @@
+public class Printing
+{
+    public static void main(String[] args)
+    {
+        // int, double, boolean, String
+        int myAge = 999;    // int is for whole numbers; integers
+        double price = 4.99;
+        boolean isGood; // variable declaration
+        isGood = false; // variable initialization
+
+        String name = "Jacquelyn Phan"; // set name to "Jacquelyn Phan"
+
+        System.out.print("Hello my name is " + name);
+    }
+}
